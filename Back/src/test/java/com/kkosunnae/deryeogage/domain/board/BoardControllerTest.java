96c599486d71a24a6fcc89613f8796642362fcf3
정동교 <jdk8467@naver.com>
2024-04-17 20:48:51 +0900

    feat: 게시글 테스트 Mockito JUnit5 연동

--- Back/src/test/java/com/kkosunnae/deryeogage/domain/board/BoardControllerTest.java
+++ Back/src/test/java/com/kkosunnae/deryeogage/domain/board/BoardControllerTest.java
@@ -1,12 +1,15 @@
 package com.kkosunnae.deryeogage.domain.board;
-import com.kkosunnae.deryeogage.domain.board.*;
+import com.kkosunnae.deryeogage.domain.board.dto.BoardRequest;
 import com.kkosunnae.deryeogage.global.s3file.S3FileService;
 import com.kkosunnae.deryeogage.global.util.JwtUtil;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,12 +19,13 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-
+@ExtendWith(MockitoExtension.class) //JUniit5와 Mockito 연동
 class BoardControllerTest {
 
-    @Mock
+    @Mock //가짜 객체 생성
     private JwtUtil jwtUtil;
 
     @Mock
@@ -30,7 +34,7 @@ class BoardControllerTest {
     @Mock
     private S3FileService s3FileService;
 
-    @InjectMocks
+    @InjectMocks //테스트 대상
     private BoardController boardController;
 
     @BeforeEach
@@ -38,30 +42,32 @@ class BoardControllerTest {
         MockitoAnnotations.initMocks(this);
     }
 
+    @DisplayName("게시글 저장 테스트")
     @Test
     @Transactional
     void saveBoard() {
         // given
         String authorizationHeader = "Bearer token";
-        BoardDto boardDto = new BoardDto();
+        BoardRequest request = new BoardRequest();
         List<MultipartFile> multipartFile = Collections.singletonList(mock(MultipartFile.class));
         Long userId = 1L;
         Integer boardId = 1;
         Map<String, List> nameList = Collections.singletonMap("filename", Collections.singletonList("s3filename"));
 
-        // when
+
         when(jwtUtil.getUserId("token")).thenReturn(userId);
-        when(boardService.save(boardDto)).thenReturn(boardId);
+        when(boardService.save(request, userId)).thenReturn(boardId);
         when(s3FileService.uploadFile(multipartFile)).thenReturn(nameList);
 
-        ResponseEntity<?> responseEntity = boardController.saveBoard(authorizationHeader, boardDto, multipartFile);
+        // when
+        ResponseEntity<?> responseEntity = boardController.saveBoard(authorizationHeader, request, multipartFile);
 
         // then
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(boardId, responseEntity.getBody());
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isEqualTo(boardId);
 
         verify(jwtUtil, times(1)).getUserId("token");
-        verify(boardService, times(1)).save(boardDto);
+        verify(boardService, times(1)).save(request, userId);
         verify(s3FileService, times(1)).uploadFile(multipartFile);
         verify(boardService, times(1)).saveBoardFile(boardId, nameList);
     }
